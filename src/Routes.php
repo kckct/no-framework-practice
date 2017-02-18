a3dfb2f29eaa58e8a56b6b7cfe43d532eb06289a
kckct <ss010359@gmail.com>
2017-02-18 21:57:07 +0800

    add src/Routes.php

---
+++ src/Routes.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types = 1)
+
+return [
+	['GET', 'hello-world', function () {
+		echo 'Hello World';
+	}],
+	['GET', 'hello-world3', function () {
+		echo 'Hello World3';
+	}],
+];
