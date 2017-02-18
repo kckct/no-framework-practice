e5ea30476a617762886c4ee7684222b100dab8ee
kckct <ss010359@gmail.com>
2017-02-18 23:53:58 +0800

    modify src/Routes.php call class method instead of closure modify src/Bootstrap.php refactor to parse Routes.php array

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -43,9 +43,14 @@ switch($routeInfo[0]) {
 		$response->setStatusCode(405);
 		break;
 	case \FastRoute\Dispatcher::FOUND:
-		$handler = $routeInfo[1];
+		//$handler = $routeInfo[1];
+		//$vars = $routeInfo[2];
+		//call_user_func($handler, $vars);
+		$className = $routeInfo[1][0];
+		$method = $routeInfo[1][1];
 		$vars = $routeInfo[2];
-		call_user_func($handler, $vars);
+		$class = new $className;
+		$class->$method($vars);
 		break;
 }
 
--- src/Routes.php
+++ src/Routes.php
@@ -1,10 +1,11 @@
 <?php declare(strict_types = 1);
 
 return [
-	['GET', '/hello-world', function () {
+	['GET', '/', ['App\Controllers\Homepage', 'show']],
+	/*['GET', '/hello-world', function () {
 		echo 'Hello World';
 	}],
 	['GET', '/hello-world3', function () {
 		echo 'Hello World3';
-	}],
+	}],*/
 ];
