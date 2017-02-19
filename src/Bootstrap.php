17a2c7bc927ece781a0d7872c83e6bbf6457f167
kckct <ss010359@gmail.com>
2017-02-19 23:37:17 +0800

    modify src/Bootstrap.php injecting response

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -43,13 +43,11 @@ switch($routeInfo[0]) {
 		$response->setStatusCode(405);
 		break;
 	case \FastRoute\Dispatcher::FOUND:
-		//$handler = $routeInfo[1];
-		//$vars = $routeInfo[2];
-		//call_user_func($handler, $vars);
 		$className = $routeInfo[1][0];
 		$method = $routeInfo[1][1];
 		$vars = $routeInfo[2];
-		$class = new $className;
+
+		$class = new $className($response);
 		$class->$method($vars);
 		break;
 }
