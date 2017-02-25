7c37feed3be99f19b0bde21095fb46e1565d7537
kckct <ss010359@gmail.com>
2017-02-25 17:12:35 +0800

    modify src/Controllers/Page.php, use PageReader to render in Page.html

--- src/Controllers/Page.php
+++ src/Controllers/Page.php
@@ -2,10 +2,28 @@
 
 namespace App\Controllers;
 
+use Http\Response;
+use App\Template\Renderer;
+use App\Page\PageReader;
+
 class Page
 {
+	private $response;
+	private $renderer;
+	private $pageReader;
+
+	public function __construct(Response $response, Renderer $renderer, PageReader $pageReader)
+	{
+		$this->response = $response;
+		$this->renderer = $renderer;
+		$this->pageReader = $pageReader;
+	}
+
 	public function show($params)
 	{
-		var_dump($params);
+		$slug = $params['slug'];
+		$data['content'] = $this->pageReader->readBySlug($slug);
+		$html = $this->renderer->render('Page', $data);
+		$this->response->setContent($html);
 	}
 }
