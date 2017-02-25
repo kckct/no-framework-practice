bdc5ec91b6199bfa73a87eaec3816d4959887ceb
kckct <ss010359@gmail.com>
2017-02-25 22:32:37 +0800

    modify src/Controllers/Homepage.php and templates/Homepage.html, add menu

--- src/Controllers/Homepage.php
+++ src/Controllers/Homepage.php
@@ -23,6 +23,7 @@ class Homepage
 	{
 		$data = [
 			'name' => $this->request->getParameter('name', 'stranger'),
+			'menuItems' => [['href' => '/', 'text' => 'Homepage']],
 		];
 		$html = $this->renderer->render('Homepage', $data);
 		$this->response->setContent($html);
